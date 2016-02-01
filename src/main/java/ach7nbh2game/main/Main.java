e02ea1e749d7395e528e08ec9bfdb3fc206682db
Nicholas Hanson-Holtry <nicholas.hanson.holtry@gmail.com>
2016-02-01 12:03:14 -0600

    command line arg parsing behavior

--- src/main/java/ach7nbh2game/main/Main.java
+++ src/main/java/ach7nbh2game/main/Main.java
@@ -8,15 +8,11 @@ import ach7nbh2game.server.GameServer;
 
 public class Main {
 
-    static final boolean localGame = false;
-    static final boolean isServer = true;
-    static final boolean isClient = true;
-
     public static void main (String[] args) {
 
         try {
 
-            if (localGame) {
+            if (false) {
 
                 // TODO this should be more robust
 
@@ -39,15 +35,61 @@ public class Main {
 
             } else {
 
-                if (isServer) { new GameServer(false); }
+                int numServers = 0;
+                int numClients = 0;
 
-                if (isClient) { new GameClient("Game Client", false, null); }
+                int length = args.length;
+
+                System.out.print(length + " args\n");
+                System.out.print("args = ");
+                for (String arg : args) {
+                    System.out.print(" " + arg);
+                }
+                System.out.print("\n");
+
+                if (length == 0) {
+                    numServers = 0;
+                    numClients = 1;
+                } else if (length > 1) {
+                    printUsage();
+                    System.exit(-1);
+                } else {
+                    for (char c : args[0].toCharArray()) {
+                        if (c == 's') {
+                            numServers = 1; // TODO ports?
+                        } else if (c == 'c') {
+                            numClients = 1; // TODO window focus?
+                        } else if (c != '-') {
+                            printUsage();
+                            System.exit(-1);
+                        }
+                    }
+                }
+
+                for (int i = 0; i < numServers; i++) {
+                    new GameServer(false);
+                }
+
+                for (int i = 0; i < numClients; i++) {
+                    new GameClient("Game Client", false, null);
+                }
 
             }
 
         } catch (Exception e) {
             // TODO
         }
+
+    }
+
+    private static void printUsage () {
+
+        System.err.println("Usage: java -jar cutthroat.jar -[s][c]");
+        System.err.println("    [s] will make one server");
+        System.err.println("    [c] will make one client");
+        System.err.println("    multiple s or c flags allowed");
+        System.err.println("    if no flags, default is -c");
+        System.err.println("    example: -ssccc");
 
     }
 
