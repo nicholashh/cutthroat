bac74a72f752926d15f16cd15ca5cea40502d210
Nicholas Hanson-Holtry <nicholas.hanson.holtry@gmail.com>
2016-02-22 00:02:28 -0600

    better logging; added date, time, color

--- src/main/java/ach7nbh2game/util/Logger.java
+++ src/main/java/ach7nbh2game/util/Logger.java
@@ -2,6 +2,9 @@ package ach7nbh2game.util;
 
 import ach7nbh2game.server.map.AGameActor;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Logger {
 
     private Logger () {}
@@ -11,6 +14,10 @@ public class Logger {
     public boolean loggingClient = false;
     public boolean loggingServer = false;
     public boolean loggingNetwork = false;
+
+    private final int classNameMaxLength = 15;
+    private final SimpleDateFormat dateFormat =
+            new SimpleDateFormat("[yyyy-MM-dd][HH:mm:ss.SSS]");
 
     public void log (Object host, int numTabs, String message) {
 
@@ -23,17 +30,21 @@ public class Logger {
             hostClassSimpleName = hostClass.getSimpleName();
         }
 
+        int color = -1;
         String hostClassFullName = hostClass.getName();
-        if ((loggingClient && hostClassFullName.contains(".client")) ||
-                (loggingServer && hostClassFullName.contains(".server")) ||
-                (loggingNetwork && hostClassFullName.contains(".network")) ) {
+        if (loggingClient && hostClassFullName.contains(".client")) {
+            color = 32; // green
+        } else if (loggingServer && hostClassFullName.contains(".server")) {
+            color = 34; // blue
+        } else if (loggingNetwork && hostClassFullName.contains(".network")) {
+            color = 35; // magenta
+        }
 
-            //if (numTabs == 0) {
-            //    System.out.println();
-            //}
+        if (color != -1) {
 
-            System.out.println("[" + hostClassSimpleName + "] " + (new String(
-                    new char[numTabs * tabSize]).replace("\0", " ")) + message);
+            System.out.println("\u001B[" + color + "m" + dateFormat.format(new Date()) + "[" +
+                    String.format("%" + classNameMaxLength + "s", hostClassSimpleName) + "] " +
+                    (new String(new char[numTabs * tabSize]).replace("\0", " ")) + message);
 
         }
 
